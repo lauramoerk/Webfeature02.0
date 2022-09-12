806161d4400c712aa34a5a088a7dc0a3140a774b
Laura <lauramoerk@hotmail.com>
2022-09-12 12:28:53 +0200

    images added to master

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
+document.querySelectorAll(".parallaxcontainerall").forEach(( parallaxcontainer ) => {
+    const parallaxel1 = parallaxcontainer.querySelector("p");
+    scroll(animate(parallaxel1, { y: [ 0, 800] }), { target: parallaxel1 });
+  });
+  
