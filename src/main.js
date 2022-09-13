b58fe30ebc569a3bdc948c911de22d6476b1af48
Robin Lau <Robi0279@stud.kea.dk>
2022-09-13 12:28:01 +0200

    robin hero

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
-document.querySelectorAll(".parallaxcontainerall").forEach(( parallaxcontainer ) => {
+document
+  .querySelectorAll(".parallaxcontainerall")
+  .forEach((parallaxcontainer) => {
     const parallaxel1 = parallaxcontainer.querySelector("p");
-    scroll(animate(parallaxel1, { y: [ 0, 800] }), { target: parallaxel1 });
+    scroll(animate(parallaxel1, { y: [0, 800] }), { target: parallaxel1 });
   });
-  
+
+inView(".inview", () => {
+  animate(".inview", { x: [-2000, 350] }, { duration: 2 });
+});
+
+inView(".inviewmobil", () => {
+  animate(".inviewmobil", { x: [-2000, 50] }, { duration: 1 });
+});
+
+inView(".inview2", () => {
+  animate(".inview2", { x: [+10000, 700] }, { duration: 2.5 });
+});
+
+inView(".inviewmobil2", () => {
+  animate(".inviewmobil2", { x: [+5000, 250] }, { duration: 1 });
+});
+
+inView(".staggerinview", ({ target }) => {
+  animate(
+    target.querySelectorAll("p"),
+    { x: [-2000, 0] },
+    { duration: 1, delay: stagger(1, { start: 0.25 }) }
+  );
+});
